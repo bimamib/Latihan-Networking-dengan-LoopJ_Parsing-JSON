fa3f711d66cae3ae6aa554ad57638ee022237977
bimamib <bimaprast1601@gmail.com>
2023-09-10 21:17:58 +0700

    membuat method pada MainActivity

--- app/src/main/java/com/bima/myquote/MainActivity.kt
+++ app/src/main/java/com/bima/myquote/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
