8bbd27a9a8df128a3bec62d8bdd403bff2c931f6
bimamib <bimaprast1601@gmail.com>
2023-09-10 22:26:32 +0700

    parsing data dengan JSONObject dalam method onSuccess

--- app/src/main/java/com/bima/myquote/MainActivity.kt
+++ app/src/main/java/com/bima/myquote/MainActivity.kt
@@ -2,11 +2,15 @@ package com.bima.myquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.bima.myquote.databinding.ActivityMainBinding
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +38,23 @@ class MainActivity : AppCompatActivity() {
                 responseBody: ByteArray
             ) {
                 // Jika koneksi berhasil
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val quote = responseObject.getString("en")
+                    val author = responseObject.getString("author")
+
+                    binding.tvQuote.text = quote
+                    binding.tvAuthor.text = author
+
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
             }
 
             override fun onFailure(
@@ -43,6 +64,15 @@ class MainActivity : AppCompatActivity() {
                 error: Throwable
             ) {
                 // Jika koneksi gagal
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
             }
         })
     }
