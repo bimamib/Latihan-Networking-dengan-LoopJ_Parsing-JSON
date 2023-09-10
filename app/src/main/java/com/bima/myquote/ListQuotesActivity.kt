4e6deca7b7c0d0188cee589736f99bc0fcf041e3
bimamib <bimaprast1601@gmail.com>
2023-09-10 23:39:05 +0700

    membuat method getListQuotes

--- app/src/main/java/com/bima/myquote/ListQuotesActivity.kt
+++ app/src/main/java/com/bima/myquote/ListQuotesActivity.kt
@@ -2,10 +2,67 @@ package com.bima.myquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bima.myquote.databinding.ActivityListQuotesBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 
 class ListQuotesActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = ListQuotesActivity::class.java.simpleName
+    }
+
+    private lateinit var binding: ActivityListQuotesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_quotes)
+        binding = ActivityListQuotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "List of Quotes"
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.listQuotes.setLayoutManager(layoutManager)
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.listQuotes.addItemDecoration(itemDecoration)
+
+        getListQuotes()
+    }
+
+    private fun getListQuotes() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/list"
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                // Jika koneksi berhasil
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                // Jika koneksi gagal
+                binding.progressBar.visibility = View.INVISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@ListQuotesActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
